08bbb7759196c9dde7ecc890ce9ad7abbd87013a
ezzedin779 <abdou.babchia64@gmail.com>
2020-09-16 22:12:32 +0100

    first prog

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,9 @@
 /**
  * main - void
  * Return: 1
- * unistd - printing in our way
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
