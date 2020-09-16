1ee487940b5b660a77d526dcd9656c8656cc70b6
ezzedin779 <abdou.babchia64@gmail.com>
2020-09-16 20:28:20 +0100

    new way to print

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-write(2,"and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 57);
+write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 57);
 return (1);
 }
