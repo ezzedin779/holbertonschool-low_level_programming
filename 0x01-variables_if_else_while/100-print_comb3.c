30ccc0b4ae7e795afc55f0c67031fafd1288ff0c
ezzedin779 <abdou.babchia64@gmail.com>
2020-09-17 19:11:09 +0100

    diff combo x2

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* main - void
+* putchar - print all combinations of double-digit numbers
+* Return: 0
+*/
+int main(void)
+{
+int a;
+for (a = 0; a <= 99; ++a)
+{
+if ((a % 10) > (a / 10))
+{
+putchar ((a / 10) + '0');
+putchar ((a % 10) + '0');
+if (a != 89)
+{
+putchar (',');
+putchar (' ');
+}
+}
+}
+putchar ('\n');
+return (0);
+}
