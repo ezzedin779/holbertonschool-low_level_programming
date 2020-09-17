9fe7cba04fb7ba8c5c5deadb78db3c41df3d38eb
ezzedin779 <abdou.babchia64@gmail.com>
2020-09-17 16:51:35 +0100

    print the alphabetics

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - void
+* putchar - print the alphabetics
+* Return: 0
+*/
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; ++a)
+{
+putchar (a);
+}
+putchar ('\n');
+return (0);
+}
