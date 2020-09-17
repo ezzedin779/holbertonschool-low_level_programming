533480cd6493e1e2438e1b35b408312cf8d1d574
ezzedin779 <abdou.babchia64@gmail.com>
2020-09-17 18:12:17 +0100

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - void
+* putchar - print the base16
+* Return: 0
+*/
+int main(void)
+{
+char a;
+for (a = '0'; a <= '9'; ++a)
+{
+putchar (a);
+}
+for (a = 'a'; a <= 'f'; ++a)
+{
+putchar (a);
+}
+putchar ('\n');
+return (0);
+}
