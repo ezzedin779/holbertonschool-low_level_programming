ff33534521f311b9a4b90856e3128f1478fe0cd4
ezzedin779 <abdou.babchia64@gmail.com>
2020-09-17 17:30:39 +0100

    hi numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - void
+* putchar - print the alphabetics
+* Return: 0
+*/
+int main(void)
+{
+int a;
+for (a = 1; a <= 9; ++a)
+{
+printf("%d", a);
+}
+printf("\n");
+return (0);
+}
