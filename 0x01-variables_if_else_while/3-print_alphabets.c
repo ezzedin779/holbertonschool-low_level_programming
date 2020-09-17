77e7ed9dbb290b367be66aa3a12633d5ada012b0
ezzedin779 <abdou.babchia64@gmail.com>
2020-09-17 17:17:12 +0100

    lower and upper alphabetics

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - void
+* putchar - print the alphabetics majus and minus
+* Return: 0
+*/
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; ++a)
+{
+putchar (a);
+}
+for (a = 'A'; a <= 'Z'; ++a)
+{
+putchar (a);
+}
+putchar ('\n');
+return (0);
+}
