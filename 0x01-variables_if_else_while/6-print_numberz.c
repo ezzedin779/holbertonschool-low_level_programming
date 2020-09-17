cddaa4c09308ddec7d87394b1ab1aa1f916050b7
ezzedin779 <abdou.babchia64@gmail.com>
2020-09-17 17:42:12 +0100

    numberzz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - void
+* putchar - print the numberz
+* Return: 0
+*/
+int main(void)
+{
+int a;
+for (a = '0'; a <= '9'; ++a)
+{
+putchar (a);
+}
+putchar ('\n');
+return (0);
+}
