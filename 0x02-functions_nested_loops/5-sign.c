4364178d201d320d22472d4ca29d1df4f43533c0
ezzedin779 <abdou.babchia64@gmail.com>
2020-09-21 16:13:06 +0100

    what's your sign ?

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+*print_sign - checking your sign
+*@n: integer
+*Return: integer
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar ('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar ('0');
+		return (0);
+	}
+	else
+	{
+		_putchar ('-');
+		return (-1);
+	}
+}
