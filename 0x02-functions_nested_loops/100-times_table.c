e21062f70618b67c3e12a18df5f28bab8d5d3315
ezzedin779 <abdou.babchia64@gmail.com>
2020-09-22 17:24:26 +0100

    done ?

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,14 +2,14 @@
 /**
 *print_times_table - time table 15
 *@n : integer
-*Return: 1
+*Return: void
 */
 void print_times_table(int n)
 {
 	int i;
 	int j;
 
-if ((n > 0) && (n < 15))
+if ((n >= 0) && (n <= 15))
 {
 	for (i = 0; i <= n; i++)
 	{
