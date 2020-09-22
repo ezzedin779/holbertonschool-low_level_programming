929c0d5e24596028e171b101b426513705b1e8c7
ezzedin779 <abdou.babchia64@gmail.com>
2020-09-22 22:17:07 +0100

    sum of even

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,16 +10,12 @@ int main(void)
 
 	for (i = 1; i <= 4000000; ++i)
 	{
-		if (i % 2 != 0)
-		{
-		if (i != 4000000)
-		printf("%d, ", x);
-		else
-		printf("%d\n", x);
 		z = x + y;
 		x = y;
 		y = z;
-		}
+		if (z % 2 == 0)
+		sum = sum + z;
 	}
+	printf("%d", sum)
 	return (0);
 }
