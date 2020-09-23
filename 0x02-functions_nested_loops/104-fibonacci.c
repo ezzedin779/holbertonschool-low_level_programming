21e68bd1a7d9b1a8926cf4de32b3fd39ee98d508
ezzedin779 <abdou.babchia64@gmail.com>
2020-09-23 17:38:06 +0100

    1 and 2

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - fib 4000000
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	unsigned long x = 0, y = 1, z = 0, s = 0;
+
+	while (z < 98)
+	{
+		z = x + y;
+		s = z;
+		while (s >= 10)
+			s /= 10;
+		if ((s == 1) || (s == 2))
+			printf ("%ld, ", z);
+		x = y;
+		y = z;
+	}
+	return (0);
+}
