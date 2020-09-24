249070f1a0f7b3a99a14cd9abc84f79117f1f7a5
ezzedin779 <abdou.babchia64@gmail.com>
2020-09-24 10:38:53 +0100

    how to do fizz buzz to 100 ?

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+*main - void
+*FizzBuzz printing
+*Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 15) == 0)
+			printf("FizzBuzz ");
+		else if (i == 100)
+			printf("Buzz\n");
+		else if ((i % 5) == 0)
+			printf("Buzz ");
+		else if ((i % 3) == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", i);
+	}
+}
