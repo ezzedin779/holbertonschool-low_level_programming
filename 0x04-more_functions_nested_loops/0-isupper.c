eecc51bccd9cc7a031c87491283ef15f40a82ed1
ezzedin779 <abdou.babchia64@gmail.com>
2020-09-24 07:48:32 +0100

    typical task to check for uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/* main - char
+*@c: checking if he's uppercase
+*Return: integer
+*/
+int main (char c)
+{
+	if ((c >= 'A') && (c <= 'Z'))
+	return (1);
+	else
+	return (0);
+}
