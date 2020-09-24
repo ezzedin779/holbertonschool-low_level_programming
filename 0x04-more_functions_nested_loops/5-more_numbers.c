523c10b756388202b71951d30313d9a893614539
ezzedin779 <abdou.babchia64@gmail.com>
2020-09-24 09:03:08 +0100

    one number twice ?

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+*more_numbers - learning to count to 14 10 times
+*
+*/
+void more_numbers(void)
+{
+	int i,j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar (j / 10 + '0');
+			_putchar (j % 10 + '0');
+		}
+	_putchar ('\n');
+	}
+}
