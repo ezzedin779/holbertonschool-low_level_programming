68947ca928959cfde9f704d074f2477fc567a030
ezzedin779 <abdou.babchia64@gmail.com>
2020-09-24 10:16:24 +0100

    diagonal printing maybe ?

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+*print_diagonal - printing lines
+*@n: number of lines ?
+*/
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 2; j <= i; j++)
+				_putchar (' ');
+			_putchar ('\\');
+			_putchar ('\n');
+		}
+	}
+	else
+	_putchar ('\n');
+}
