78ad13b28163a9a8f3d088e6d86d8cd07f2b458b
ezzedin779 <abdou.babchia64@gmail.com>
2020-09-28 19:35:16 +0100

    half done ?

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+*puts_half - prints half a string
+*@str: string
+*/
+void puts_half(char *str)
+{
+	int i, j, n;
+
+	for (i = 0; str[i]; i++)
+	{}
+	if (i % 2 == 0)
+	{
+		n = i / 2;
+		for (j = n; j < i; j++)
+		{
+			_putchar (str[j]);
+		}
+	}
+	else 
+	{
+		n = (i - 1) / 2;
+		for (j = n; j < i; j++)
+		{
+			_putchar (str[j]);
+		}
+	}
+	_putchar ('\n');
+}
