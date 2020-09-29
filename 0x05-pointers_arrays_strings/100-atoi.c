f5e6385ceeb2aee810ffefd1d883669f87a517f0
ezzedin779 <abdou.babchia64@gmail.com>
2020-09-29 15:07:24 +0100

    get me my numbers please ?

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,19 @@
+/**
+*_atoi - gets the number out
+*Return: integer
+*/
+int _atoi(char *s)
+{
+	int i = 0, num = 0, neg = -1, lnum = 0;
+
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+	{
+		if (s[i] == '-')
+		neg *= -1;
+		i++;
+	}
+	while (s[i] != '\0' && (s[i] >= '0' && s[i] <= '9'))
+		num = (num * 10) - (s[i++] - '0');
+	lnum = neg * num;
+	return (lnum);
+}
