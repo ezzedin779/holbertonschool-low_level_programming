ea64f87459c7b88d1db35d816e317e062fc8a903
ezzedin779 <abdou.babchia64@gmail.com>
2020-10-01 13:03:56 +0100

    copy n byte

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,7 +9,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i, j;
 
-	for (i = 0; i <= n && src[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 	for (j = i; j < n; j++)
 		dest[j] = '\0';
