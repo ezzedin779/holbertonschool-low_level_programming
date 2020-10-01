a0eb67a3e73fd42b0144943b8f03a3ae386e5041
ezzedin779 <abdou.babchia64@gmail.com>
2020-10-01 12:15:31 +0100

    copy n byte

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,17 @@
+/**
+*_strncpy - copies n byte from a string to another one
+*@dest: Destination
+*@src: source
+*@n: number of bytes
+*Return: char (source in the destination)
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for (j = i; j < n; j++)
+		dest[j] = '\0';
+	return (dest);
+}
