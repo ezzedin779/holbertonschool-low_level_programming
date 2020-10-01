69ccd3707de53eae08f2d226d1046ae60f7a8d9f
ezzedin779 <abdou.babchia64@gmail.com>
2020-10-01 12:15:17 +0100

    cat n byte

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+/**
+*_strncat - concatenates two strings with n bytes
+*@dest: Destination
+*@src: source
+*@n: number of bytes
+*Return: char (source in the end of destination)
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0, k;
+
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+		j++;
+	if (j < n)
+	{
+		for (k = 0; k <= j; k++)
+		{
+			dest[i] = src[k];
+			i++;
+		}
+	}
+	else
+	{
+		for (k = 0; k <= n; k++)
+		{
+			dest[i] = src[k];
+			i++;
+		}
+	}
+	return (dest);
+}
