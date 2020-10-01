2c36b9b2020ade6d38741d19b1fa5763d9324d2b
ezzedin779 <abdou.babchia64@gmail.com>
2020-10-01 15:26:39 +0100

    uppercase only

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+/**
+*string_toupper - uppercase
+*@a: char
+*Return: integer
+*/
+char *string_toupper(char *a)
+{
+	int i;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		if (a[i] >= 'a' && a[i] <= 'z')
+			a[i] -= 32;
+	}
+	return (a);
+}
