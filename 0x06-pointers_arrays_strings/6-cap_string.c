308b1a7dcd62f930a1d57261b9378ac7e429edfc
ezzedin779 <abdou.babchia64@gmail.com>
2020-10-01 16:18:22 +0100

    uppercase every first letter ?

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+/**
+*cap_string - capitalise letters
+*@a: string
+*Return: char
+*/
+char *cap_string(char *a)
+{
+	int i = 0, j;
+	char ch[] = " \t\n,;.!?\"(){}";
+
+	while (a[i] != '\0')
+	{
+		if (a[i] >= 'a' && a[i] <= 'z')
+		{
+			if (i == 0)
+				a[i] -= 32;
+			else
+			{
+				for (j = 0; ch[j] != '\0'; j++)
+				{
+					if (ch[j] == a[i - 1])
+						a[i] -= 32;
+				}
+			}
+		}
+		i++;
+	}
+	return (a);
+}
