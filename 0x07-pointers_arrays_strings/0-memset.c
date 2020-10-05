033d6073cc014f64c9cfc6cdd243725ba7c433c6
ezzedin779 <abdou.babchia64@gmail.com>
2020-10-05 10:22:16 +0100

    filling n bytes

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+/**
+*_memset - fills n bytes of a memory with b a constant
+*@s: pointer to the area
+*@b: constant to fill with
+*@n: number of bytes to fill
+*Return: filled pointer
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
