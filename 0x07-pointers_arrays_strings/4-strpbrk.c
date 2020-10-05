910ad88709b7d27bce4e09f434f10f11d155c6b6
ezzedin779 <abdou.babchia64@gmail.com>
2020-10-05 10:23:40 +0100

    locating first occurence in a string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,20 @@
+/**
+*_strpbrk - locate the first occurrence in a string
+*@s: source
+*@accept: searched in source
+*Return: pointer to the first occurrence
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (&s[i]);
+		}
+	}
+	return (0);
+}
