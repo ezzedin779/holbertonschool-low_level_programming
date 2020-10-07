bdc8b20840986d99f3082bb9a73558909536ee70
ezzedin779 <abdou.babchia64@gmail.com>
2020-10-07 11:11:16 +0100

    calculating factorial (recursion method)

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,13 @@
+/**
+*factorial - calculating factorial
+*@n: number to do factorial
+*Return: factorial of n
+*/
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n <= 1)
+		return (1);
+	return (n * factorial(n - 1));
+}
