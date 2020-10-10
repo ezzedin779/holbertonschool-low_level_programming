2a765db648b3815bce3ab95869a60dac3b677089
ezzedin779 <abdou.babchia64@gmail.com>
2020-10-10 19:18:21 +0100

    printing function name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,8 +5,8 @@
 *@argv: array of strings
 *Return: 0
 */
-int main(int argc __attribute__((unused)), char **argv)
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", *argv);
+	printf("%s\n", argv[0]);
 	return (0);
 }
