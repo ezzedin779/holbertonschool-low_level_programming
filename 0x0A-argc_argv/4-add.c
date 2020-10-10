addadf8ec369a97792e2efb65be9f3c63deb9e66
ezzedin779 <abdou.babchia64@gmail.com>
2020-10-10 20:34:51 +0100

    adding arguments

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - printing the number of arguments passed
+*@argc: length of argv
+*@argv: array of strings
+*Return: 1
+*/
+int main(int argc, char *argv[])
+{
+	int x = 0, var = 0, i;
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) != 0)
+		{
+			var = atoi(argv[i]);
+			x += var;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf ("%d\n", x);
+	return (0);
+}
