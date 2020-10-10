6bf86594f9f6a0ecdbe26b44f1e7e595546b936f
ezzedin779 <abdou.babchia64@gmail.com>
2020-10-10 21:01:36 +0100

    adding arguments

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,22 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 /**
-*main - printing the number of arguments passed
+*checker - checking if it's a number or not
+*@a: array of strings
+*Return: integer
+*/
+int checker(char *a)
+{
+	int i;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		if (isdigit(a[i]) == 0)
+			return (0);
+	}
+	return (1);
+}
+/**
+*main - adding 2 numbers
 *@argc: length of argv
 *@argv: array of strings
 *Return: 1
 */
 int main(int argc, char *argv[])
 {
-	int x = 0, var = 0, i;
+	int x = 0, i;
 
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]) != 0)
-		{
-			var = atoi(argv[i]);
-			x += var;
-		}
+		if (checker(argv[i]) == 1)
+			x += atoi(argv[i]);
 		else
 		{
 			printf("Error\n");
