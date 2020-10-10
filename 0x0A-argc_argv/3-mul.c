caf51ec6743389a4fb407ce89fda651e276adae8
ezzedin779 <abdou.babchia64@gmail.com>
2020-10-10 20:06:50 +0100

    multiplication between arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - printing the number of arguments passed
+*@argc: length of argv
+*@argv: array of strings
+*Return: 1
+*/
+int main(int argc, char *argv[])
+{
+	int x = 0,y = 0;
+	if (argc == 3)
+	{
+		x = atoi(argv[1]);
+		y = atoi(argv[2]);
+		printf("%d\n", x * y);
+	}
+	else
+		printf("Error\n");
+	return (1);
+}
