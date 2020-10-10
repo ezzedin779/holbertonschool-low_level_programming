c0843791526710c19e487c40adcfba72febabe44
ezzedin779 <abdou.babchia64@gmail.com>
2020-10-10 19:14:01 +0100

    printing function name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-#include "holberton.h"
 /**
 *main - printing the name of the program
 *@argc: length of argv
 *@argv: array of strings
 *Return: 0
 */
-int main(__attribute__((unused))int argc, char *argv[])
+int main(int argc __attribute__((unused)), char **argv[])
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", *argv);
 	return (0);
 }
