068ce8bd04732064704fdffb106225da768286f6
ezzedin779 <abdou.babchia64@gmail.com>
2020-10-13 20:50:23 +0100

    concatention of 2 strings in a new string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,8 +23,10 @@ char *str_concat(char *s1, char *s2)
 	int i;
 	char *ch;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	ch = malloc(sizeof(char) * (length(s1) + length(s2) + 1));
 	if (ch == NULL)
 		return (NULL);
