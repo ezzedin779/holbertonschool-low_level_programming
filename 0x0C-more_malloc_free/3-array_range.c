1909fd8ccf8e2cbede2e4e51e6bcde3c345c2c69
ezzedin779 <abdou.babchia64@gmail.com>
2020-10-14 20:40:06 +0100

    creating ranged array

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,7 +13,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 	size = max - min + 1;
-	ar = calloc(size, sizeof(int) * size);
+	ar = malloc(sizeof(int) * size);
 	if (ar == NULL)
 		return (NULL);
 	for (i = 0; i <= size && min <= max; i++, min++)
