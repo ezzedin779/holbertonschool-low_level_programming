fc10050550b73e5207abec9f60c492622f2bad41
ezzedin779 <abdou.babchia64@gmail.com>
2020-10-14 20:23:42 +0100

    creating calloc with malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,14 +8,16 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
-	char *p;
+	void *p1;
+	int *p2;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	p = malloc(nmemb * size);
-	if (p == NULL)
+	p1 = malloc(nmemb * size);
+	if (p1 == NULL)
 		return (NULL);
+	p2 = p1;
 	for (i = 0; i != size; i++)
-		*(p + (size * i)) = 0;
-	return (p);
+		*(p2 + (size * i)) = 0;
+	return (p1);
 }
