3a2f96d57d4fe204c3e857b9e3cbbccb0aef61aa
ezzedin779 <abdou.babchia64@gmail.com>
2020-10-19 20:32:43 +0100

    declaring a structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+*struct dog - structure of dog
+*@name: the name of the dog
+*@age: how old is he ?
+*@owner: who's your owner ?
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
