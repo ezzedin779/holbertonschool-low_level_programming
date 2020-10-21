af7c50aaf84e1132add76fedeb331820279dfc34
ezzedin779 <abdou.babchia64@gmail.com>
2020-10-21 08:20:40 +0100

    applying a function to all the array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+/**
+*array_iterator - executes action on every part in the array
+*@array: pointer to an array
+*@size: the size of an array
+*@action: funtion pointer to the action to do
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i =0; i < size; i++)
+		action(array[i]);
+}
