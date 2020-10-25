fd7f4ec288ffb03c4cf7e0befc9b69b0d9ae3a19
ezzedin779 <abdou.babchia64@gmail.com>
2020-10-25 10:25:26 +0100

    printing all numbers in args of a function

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,28 +9,18 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list list_num;
 	unsigned int i;
+	int x;
 
 	if (n == 0)
 		return;
 	va_start(list_num, n);
 	for (i = 0; i < n; i++)
 	{
-		if (separator == NULL)
-		{
-			if (n - 1 != i)
-				printf("%d", va_arg(list_num, int));
-			else
-				printf("%d\n", va_arg(list_num, int));
-		}
-		else
-		{
-			if (i == 0)
-				printf("%d", va_arg(list_num, int));
-			else if (n - 1 != i)
-				printf("%s%d", separator, va_arg(list_num, int));
-			else if (n - 1 == i)
-				printf("%s%d\n", separator, va_arg(list_num, int));
-		}
+		x = va_arg(list_num, int);
+			printf("%d", x);
+		if (i != n - 1 && separator != NULL)
+			printf("%s", separator);
 	}
+	printf("\n");
 	va_end(list_num);
 }
