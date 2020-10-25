efb3dfc997952f1e995fc8473b729ba5572fb26a
ezzedin779 <abdou.babchia64@gmail.com>
2020-10-25 11:03:22 +0100

    printing all numbers in args of a function

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,8 +10,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list list_num;
 	unsigned int i;
 
-	if (n == 0)
-		return;
 	va_start(list_num, n);
 	for (i = 0; i < n; i++)
 	{
