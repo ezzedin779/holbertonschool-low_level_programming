1d6b552f029768a6a78e530f358597f4c565bd59
ezzedin779 <abdou.babchia64@gmail.com>
2020-10-29 11:20:20 +0100

    printing before execution

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+*print_before - printing a msg before the main program
+*/
+void print_before(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+	I bore my house upon my back!\n");
+}
