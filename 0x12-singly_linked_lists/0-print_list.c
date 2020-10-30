b04f33ab9ff7516f56fecc2361fd6cb48fa34984
ezzedin779 <abdou.babchia64@gmail.com>
2020-10-30 19:57:11 +0100

    printing lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,7 +13,7 @@ size_t print_list(const list_t *h)
 	for (i = 0; h != NULL; i++)
 	{
 		if (n->str != NULL)
-			printf("[%lu] %s", h->len, h->str);
+			printf("[%d] %s", h->len, h->str);
 		else
 			printf("[0] (nil)");
 		n = n->next;
