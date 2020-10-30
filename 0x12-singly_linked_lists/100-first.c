33ae8fbcc71cef595e2b21efb7ae2620b9ae90de
ezzedin779 <abdou.babchia64@gmail.com>
2020-10-30 20:07:00 +0100

    executing function before the main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,6 +4,7 @@
 */
 void print_before(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-	I bore my house upon my back!\n");
+	char *s1 = "You're beat! and yet, you must allow,\n";
+
+	printf("%sI bore my house upon my back!\n", s1);
 }
