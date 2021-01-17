9a3afd637141ce67af2e09c56a9ce7aa9d539b62
ezzedin779 <abdou.babchia64@gmail.com>
2021-01-17 21:16:29 +0100

    CREATE

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,7 +6,7 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash;
+	hash_table_t *hash = NULL;
 	unsigned long int i;
 
 	hash = malloc(sizeof(hash_table_t));
@@ -15,7 +15,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash->size = size;
 	hash->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash->array == NULL)
+	{
+		free(hash);
 		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 		hash->array[i] = NULL;
 	return (hash);
