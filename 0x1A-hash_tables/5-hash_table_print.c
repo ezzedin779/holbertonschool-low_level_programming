f0b71bd8ce9f939c8ed9465570de009020a12b71
ezzedin779 <abdou.babchia64@gmail.com>
2021-01-17 22:19:47 +0100

    printing hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -21,7 +21,7 @@ void hash_table_print(const hash_table_t *ht)
 		while (ht->array[i] != NULL)
 		{
 			if (signal == 1)
-				print(", ");
+				printf(", ");
 			printf("'%s': '%s'", (ht->array[i])->key, (ht->array[i])->value);
 			signal = 1;
 			ht->array[i] = (ht->array[i])->next;
