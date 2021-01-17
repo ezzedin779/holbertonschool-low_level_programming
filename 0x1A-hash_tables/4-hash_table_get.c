9ea4ae5a11743f3d9c8d7b6880885110c3657829
ezzedin779 <abdou.babchia64@gmail.com>
2021-01-17 21:47:26 +0100

    GET that element

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -16,11 +16,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	index = key_index((unsigned char *)key, ht->size);
 	if (index >= ht->size)
 		return (NULL);
-	node = ht->array[index];
-	while (node && strcmp(node->key, key) != 0)
-		node = node->next;
-	if (node == NULL)
+	new_node = ht->array[index];
+	while (new_node && strcmp(new_node->key, key) != 0)
+		new_node = new_node->next;
+	if (new_node == NULL)
 		return (value);
-	value = node->value;
+	value = new_node->value;
 	return (value);
 }
