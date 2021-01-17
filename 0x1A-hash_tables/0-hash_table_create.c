0165a0bec9c59447a73ef3e9e9d3b7bc689a927d
ezzedin779 <abdou.babchia64@gmail.com>
2021-01-17 21:19:11 +0100

    CREATE

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,7 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (hash == NULL)
 		return (NULL);
 	hash->size = size;
-	hash->array = malloc(sizeof(hash_node_t *) * size);
+	hash->array = malloc(sizeof(hash_node_t) * size);
 	if (hash->array == NULL)
 	{
 		free(hash);
