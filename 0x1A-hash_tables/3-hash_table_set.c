09869f5fb2059d0a1985d75a09f4ef067be8a45f
ezzedin779 <abdou.babchia64@gmail.com>
2021-01-17 21:30:34 +0100

    SET

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,18 +13,18 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index, i;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-		return(0);
+		return (0);
 	copy = strdup(value);
 	if (copy == NULL)
 		return (0);
-	index = key_index((const unsigned char*)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 	for (i = index; ht->array[i]; i++)
 	{
 		if (strcmp(ht->array[i]->key, key) == 0)
 		{
 			free(ht->array[i]->value);
 			ht->array[i]->value = copy;
-			return (1)
+			return (1);
 		}
 	}
 	node = malloc(sizeof(hash_node_t));
@@ -34,7 +34,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 	node->key = strdup(key);
-	if(node->key == NULL)
+	if (node->key == NULL)
 	{
 		free(node);
 		return (0);
